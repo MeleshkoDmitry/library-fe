45e566a0aaffc702da14943173a54323bb1b513d
INTEXSOFT\dmitry.mialeshka <dmitry.mialeshka@intexsoft.by>
2018-07-17 14:00:00 +0300

    Reworked pagination component

--- src/app/common/pagination/pagination.component.ts
+++ src/app/common/pagination/pagination.component.ts
@@ -9,32 +9,55 @@ import { Pagination } from '../../book/book';
 })
 
 export class PaginationComponent implements OnChanges {
-  totalPages: number;
-  selectPageSize = [1, 5, 10, 15];
 
+  selectPageSize = [1, 5, 10, 15];
   @Input() pagination: Pagination;
   @Output() pageEvent: EventEmitter<any> = new EventEmitter<any>();
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.pagination.currentValue && !changes.pagination.firstChange) {
-      this.createTotalPages();
+      this.totalPages();
     }
   }
 
-  createTotalPages() {
-    this.totalPages = Math.ceil(this.pagination.totalRecords / this.pagination.pageSize);
+  totalPages(): number {
+    return Math.ceil(this.pagination.totalRecords / this.pagination.pageSize) || 0;
   }
 
-  next(): void {
+  onNext(): void {
     this.pageEvent.emit({ page: this.pagination.page + 1, pageSize: this.pagination.pageSize });
   }
 
-  prev(): void {
+  onPrev(): void {
     this.pageEvent.emit({ page: this.pagination.page - 1, pageSize: this.pagination.pageSize });
   }
 
   changeValue(): void {
-    this.createTotalPages();
+    this.totalPages();
     this.pageEvent.emit({ page: 1, pageSize: this.pagination.pageSize });
+  }
+
+  getPages(): number[] {
+    const pages: number[] = [this.pagination.page];
+    const pagesToShow = 5;
+
+    for (let i = 0; i < pagesToShow; i++) {
+      if (pages.length < pagesToShow) {
+        if (Math.min(...pages) > 1) {
+          pages.push(Math.min(...pages) - 1);
+        }
+      }
+      if (pages.length < pagesToShow) {
+        if (Math.max(...pages) < this.totalPages()) {
+          pages.push(Math.max(...pages) + 1);
+        }
+      }
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
+  }
+
+  onPage(pageNumber: number): void {
+    this.pageEvent.emit({ page: pageNumber, pageSize: this.pagination.pageSize });
   }
 }
